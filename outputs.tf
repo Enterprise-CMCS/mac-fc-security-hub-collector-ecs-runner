a908d2ed1a3708e4bbe2803e9a013711135874e1
ben-harvey <benjaminriderharvey@gmail.com>
2025-12-11 14:24:07 -0500

    Add task def arn output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "task_security_group_id" {
   description = "Security group of the ECS task"
   value       = aws_security_group.ecs_sg.id
 }
+
+output "task_definition_arn" {
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.scheduled_task_def.arn
+}
